aabf30d90812f8d0a2696a53d6725a445aa9c365
suraiya-jahan-bhuiyan-sraboni <suraiya.jaha.bhuiyan.12@gmail.com>
2025-05-22 16:21:13 +0600

    fixing small things

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -66,30 +66,6 @@ const Register = () => {
       });
 
   }
-  const handleGoogleLogin = () => {
-    loginWithGoogle().then((result) => {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-      // The signed-in user info.
-      const user = result.user;
-      // IdP data available using getAdditionalUserInfo(result)
-      //console.log(result);
-      // ...
-  
-      toast.success("Logged In Sucessful")
-    }).catch((error) => {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.customData.email;
-      // The AuthCredential type that was used.
-      const credential = GoogleAuthProvider.credentialFromError(error);
-      // ...
-      toast.error(errorMessage);
-    });
-  }
 
   return (
     <div>
@@ -139,17 +115,7 @@ const Register = () => {
                 <button type="submit" className="w-full px-8 py-3 font-semibold rounded-md dark:bg-violet-600 dark:text-gray-50">Register</button>
               </div>
               <p className='text-center'>Already Have An Account? <br /> <Link to="/login" className='hover:underline text-blue-800'>Login Here</Link> </p>
-              <div className="flex items-center w-full my-4">
-                <hr className="w-full dark:text-gray-600" />
-                <p className="px-3 dark:text-gray-600">OR</p>
-                <hr className="w-full dark:text-gray-600" />
-              </div>
-              <button onClick={handleGoogleLogin} aria-label="Login with Google" type="button" className="flex items-center justify-center w-full p-4 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:border-gray-600 focus:dark:ring-violet-600">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-5 h-5 fill-current">
-                  <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
-                </svg>
-                <p>Login with Google</p>
-              </button>
+
             </div>
           </form>
         </div>
